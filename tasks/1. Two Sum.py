992c1340627993ad301d559737728b920f20b5d6
Anatol <seddboom@gmail.com>
2026-08-12 23:46:57 +0200

    Update 1. Two Sum.py

--- tasks/1. Two Sum.py
+++ tasks/1. Two Sum.py
@@ -1,7 +1,7 @@
 # Task Description
 # Given an array of integers nums(not sorted) and an integer target, return indices of the two numbers such that they
 # add up to target. You may assume that each input would have exactly one solution, and you may not use the same
-# element twice. You can return the answer in any order.
+# element twice. You can return the answer in any order. 
 
 # Example:
 # Input: nums = [2, 7, 11, 15], target = 9
